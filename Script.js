736682e05217029181572397607f4ff7e64c6fbc
alicedesouzabr-lab <alicedesouza.br@gmail.com>
2025-12-10 00:37:16 -0300

    Delete Script.js

--- Script.js
+++
@@ -1,9 +0,0 @@
-// Toggle menu mobile
-document.addEventListener('DOMContentLoaded', function() {
-    const hamburger = document.getElementById('hamburger');
-    const navMenu = document.getElementById('nav-menu');
-
-    hamburger.addEventListener('click', function() {
-        navMenu.classList.toggle('active');
-    });
-});
